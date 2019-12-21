12c7f62a4aac76cb9504e23a3763f1b5bd57cc11
rajesh5696 <rajeshece052.com>
2019-12-21 20:45:34 +0530

    port changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT||3000
+const port = process.env.PORT||80
 // const port =3000
 var mongoose = require('mongoose')
 var userProfilesModal=require('./UserProfile/userProfile');
