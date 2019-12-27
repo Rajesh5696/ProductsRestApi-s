35c8e4edfc051d807696b98ed0400f2c6fdb25cb
GCS-DT-109\rthotakura <rajeshece052@email.com>
2019-12-27 11:24:57 +0530

    recent

--- app.js
+++ app.js
@@ -30,19 +30,8 @@ app.get('/', (req, res) => {
 
 
 function ConnecttoMonoDb(){
-
-<<<<<<< HEAD
-
-
     mongoose.connect('mongodb://52.66.166.185/admin:27017',(err,client)=>{
 
-    // mongoose.connect('mongodb:52.66.166.185:27017',(err,client)=>{
-
-=======
-    mongoose.set('useNewUrlParser', true);
-    mongoose.set('useUnifiedTopology', true);
-    mongoose.connect('mongodb://52.66.166.185:27017', { useNewUrlParser: true },(err,client)=>{
->>>>>>> 1b2d6ed96d3719b0124ee504e1fe1334887a23e9
         if(err){
             return console.log('unable to connect mongodb',err)
             res.send('sorry!')
